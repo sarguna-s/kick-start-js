425af9795cb09abb5bc7cfa21b32ff234f86fa94
Sarguna S <sargunasamidurai@gmail.com>
2024-07-26 14:21:27 +0530

    simple task

---
+++ keyword.js
@@ -0,0 +1,28 @@
+// let x = 9;
+// var y = 7;
+// const z = x + y;
+// console.log(z);
+
+// const person = "sarguna";
+// const age = 20;
+// const address = "chennai";
+// console.log("person :", person);
+// console.log("age :", age);
+// console.log("address :", address);
+
+let x = 10;
+// const y = 12;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x + y, x - y, x * y, x / y, x % y, x ** y);
+
+// console.log((x += 5));
+// console.log((x -= 5));
+// console.log((x *= 5));
+// console.log((x /= 5));
+// console.log((x %= 5));
+// console.log((x **= 5));
+// console.log((x += 5), (x -= 5), (x *= 5), (x /= 5), (x %= 5), (x **= 5));
+console.log("10" > 9);
